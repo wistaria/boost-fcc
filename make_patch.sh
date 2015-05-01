157c10c6b3889d80c44c07810cd93cbe677af7fb
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-05-01 23:26:36 +0900

    Updated make_patch.sh script

--- make_patch.sh
+++ make_patch.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-git diff --no-prefix -r import_1_55_0 boost_1_55_0 > boost_1_55_0.patch
+VERSION_MAJOR=1_55
+VERSION_MINOR=0
+
+git diff --no-prefix -r import_$VERSION_MAJOR src | sed s^src/boost^boost_${VERSION_MAJOR}_${VERSION_MINOR}/boost^g > boost_${VERSION_MAJOR}_${VERSION_MINOR}.patch
